f98bd1480feda9a85eeb7cd6dea7ec607a8a7631
unknown <davcost@yandex.ru>
2025-07-09 12:20:27 +0300

    add LoginPage, fix previous commits

--- test_main_page.py
+++ test_main_page.py
@@ -1,3 +1,4 @@
+from .pages.login_page import LoginPage
 from .pages.main_page import MainPage
 
 
@@ -12,3 +13,23 @@ def test_guest_should_see_login_link(browser):
     page = MainPage(browser, link)
     page.open()
     page.should_be_login_link()
+
+def test_guest_should_see_login_url(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)
+    page.open()
+    page.go_to_login_page()
+    page.should_be_login_link()
+
+def test_guest_should_see_login_form(browser):
+    link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
+    page = LoginPage(browser,link)
+    page.open()
+    page.should_be_login_form()
+
+def test_guest_should_see_register_form(browser):
+    link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
+    page = LoginPage(browser,link)
+    page.open()
+    page.should_be_register_form()
+
